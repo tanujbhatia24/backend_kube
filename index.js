c41931c4b69237096189eac4e749cf565b715397
Balraj <bhullar.bs@gmail.com>
2022-11-10 21:58:44 +0530

    Login api for student

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-const connect = require('./database/mongoDb')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-require("dotenv").config();
+const port = process.env.PORT || 3000;
+const connect = require("./database/mongoDb");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+// require("dotenv").config();
+require("dotenv").config({ path: "config.env" });
 
-const studentRoutes = require('./routes/student.routes')
-app.use(cookieParser())
+const studentRoutes = require("./routes/student.routes");
+
+app.use(cookieParser());
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+app.use("/student", studentRoutes);
 
-app.use('/student',studentRoutes)
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World! check');
+app.get("/", (req, res) => {
+  res.send("Hello World! check");
 });
 
-
-
 app.listen(port, () => {
-  connect()
+  connect();
   console.log(`Example app listening at http://localhost:3000`);
 });
