9fea3ca9315b448e1ef263cb7e8d153b7abd4521
punjabi gagan <aseemgirdhar@gmail.com>
2022-11-22 15:31:50 +0530

    question upload api

---
+++ controllers/questionUpload.controller.js
@@ -0,0 +1,35 @@
+const Questions = require("../models/questionUpload.model");
+
+const addQuestion = (req, res) => {
+  const question = req.body.question;
+  
+  const newQuestion = new Questions({
+    ...req.body,
+  });
+  
+  Questions.findOne({ question }, async (err, que) => {
+
+    if (que) {
+      res.status(400).json({ message: "Question already exist" });
+      return;
+    } else if (
+      !req.body.question ||
+      !req.body.total_marks ||
+      !req.body.skill_tag ||
+      !req.body.sub_tag ||
+      !req.body.tag_level
+    ) {
+      res.status(400).json({ message: "Please fill all the details" });
+    } else {
+      try {
+        const val = await newQuestion.save();
+        res.status(200).json(val);
+      } catch (error) {
+        res.status(400).json({ message: error.message });
+      }
+    }
+  });
+
+};
+
+module.exports = { addQuestion };
